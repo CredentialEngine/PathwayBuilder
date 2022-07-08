8f68b33c5a8bef3bb28461a5fa977a855279f646
ashyad01 <ashish.yadav@protiviti.co.in>
2022-07-08 12:06:10 +0530

    created a custom drawer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import CustomDrawer from './components/customDrawer';
 
 import Header from './components/header';
 import MainContainer from './components/mainContainer';
 import RightPanel from './components/rightPanel';
 
-const App = () => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-    }}
-  >
-    <Header />
-    <MainContainer>
-      {/* <CreatePathway visible={true} /> */}
-      <RightPanel visible={false} />
-    </MainContainer>
-  </div>
-);
+const App = () => {
+  const [isrightPanelDrawerVisible, setRightPanelDrawerVisible] =
+    useState<boolean>(true);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Header />
+      <MainContainer>
+        <CustomDrawer></CustomDrawer>
+        <CustomDrawer
+          title="Transaction Review"
+          width="60%"
+          visible={isrightPanelDrawerVisible}
+          onClose={() => setRightPanelDrawerVisible(false)}
+          footer={[]}
+        >
+          <RightPanel />
+        </CustomDrawer>
+      </MainContainer>
+    </div>
+  );
+};
 
 export default App;
