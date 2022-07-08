5a37a72477ad095d68e09415afaa4d78f35388bd
Sain <sausai01@protiviti.com>
2022-07-08 10:47:45 +0800

    added right panel

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import Header from './components/header';
 import MainContainer from './components/mainContainer';
-import CreatePathway from './screens/createPathway/createPathway';
+import RightPanel from './components/rightPanel';
 
 const App = () => (
-  <div>
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+    }}
+  >
     <Header />
     <MainContainer>
-      <CreatePathway visible={true} />
+      {/* <CreatePathway visible={true} /> */}
+      <RightPanel visible={true} />
     </MainContainer>
   </div>
 );
