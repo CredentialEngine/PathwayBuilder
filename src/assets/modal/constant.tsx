3fffcda4563de8efed6ecb72492c3feaffe53a35
aakashdeep2389 <aakash.d2389@gmail.com>
2022-08-01 13:44:20 +0530

    merge conflict

--- src/assets/modal/constant.tsx
+++ src/assets/modal/constant.tsx
@@ -1,13 +1,16 @@
 export const ComponentsCards = [
   {
+    id: '9862349723864729834',
     name: 'Credentials Component',
     description: 'FOS-291 BSCH',
   },
   {
+    id: '9862349864729834',
     name: 'Course',
     description: 'F20BP Design & Implementation',
   },
   {
+    id: '09862349864729834',
     name: 'Mechanical',
     description: 'Mechanical Data Engineering',
   },
