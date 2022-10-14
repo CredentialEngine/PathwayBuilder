b95ede9dd5b08aae39eb8a7bc8a6c5e8d30cd674
aakashdeep2389 <aakash.d2389@gmail.com>
2022-10-14 12:06:18 +0530

    constraint error resolve

--- src/screens/addComponent/index.tsx
+++ src/screens/addComponent/index.tsx
@@ -100,27 +100,7 @@ const AddConditionalComponent: React.FC<Props> = (Props) => {
       ParentIdentifier: componentConditionFields.ParentIdentifier,
 
       Description: componentConditionFields.Description,
-      // LeftAction :
-      // LeftSource: constraintData.LeftSource,
-      // Comparator: componentConditionFields.Comparator,
-
-      // RightSource: constraintData.RightSource,
-
-      // rowid: 'asdasdasdasd',
-      // leftSource: [
-      //   {
-      //     URI: 'ceterms:AdvancedStandingAction',
-      //     Name: leftSourcedata,
-      //   },
-      // ],
-      // comparator: `compare:${constraintData.Comparator}`,
-      // RightAction:
-      // rightSource: [
-      //   {
-      //     URI: 'ceterms:AdvancedStandingAction',
-      //     Name: rightSourcedata,
-      //   },
-      // ],
+      constraintRow,
     };
     const ComponentCondition = {
       ParentIdentifier: componentConditionFields.ParentIdentifier,
@@ -167,6 +147,8 @@ const AddConditionalComponent: React.FC<Props> = (Props) => {
       setConstraintRow([]);
     }
   };
+  console.log(constraintRow, 'constraintRow');
+
   return (
     <div className={Styles.addComponentwrapper}>
       <Form>
