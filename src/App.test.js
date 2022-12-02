8afc3258472fc6f46cc000e3f5ead72914212668
dlaskowski <dave.laskowski@protiviti.com>
2022-12-02 10:55:02 -0600

    add Jest testing framework and trivial first unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('pages Index', () => {
+  it('should render correctly', () => {
+    expect(1).toBe(1);
+  });
 });
